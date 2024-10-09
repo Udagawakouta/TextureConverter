dd80a35c01ce43679582f78f2e01a7704e1c9311
Udagawakouta <115062885+Udagawakouta@users.noreply.github.com>
2024-10-09 11:34:45 +0900

    02_04おしまい

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -1,4 +1,5 @@
 #include "TextureConverter.h"
+#include <windows.h>
 
 TextureConverter::TextureConverter()
 {
@@ -10,13 +11,27 @@ TextureConverter::~TextureConverter()
 
 void TextureConverter::ConvertTextureWICToDDS(const std::string& filePath)
 {
+	// テクスチャファイルを読み込む
+	LoadWICTextureFromFile(filePath);
 }
 
-void TextureConverter::LoadWICTextureFromFile(const std::string& filepath)
+void TextureConverter::LoadWICTextureFromFile(const std::string& filePath)
 {
+	// ファイルパスをワイド文字列に変換する
+	std::wstring wfilePath = ConvertMultiByteStringToWideString(filePath);
 }
 
 std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::string& mString)
 {
-	return std::wstring();
+	// ワイド文字列に変換した際の文字列を計算
+	int filePathBufferSize = MultiByteToWideChar(CP_ACP, 0, mString.c_str(), -1, nullptr, 0);
+
+	// ワイド文字列
+	std::wstring wString;
+	wString.resize(filePathBufferSize);
+
+	// ワイド文字列に変換
+	MultiByteToWideChar(CP_ACP, 0, mString.c_str(), -1, &wString[0], filePathBufferSize);
+
+	return wString;
 }
